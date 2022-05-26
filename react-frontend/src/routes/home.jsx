deed00efb03244d806c0f2495625d11c720b34a2
RShumy <razvansuhan@yahoo.com>
2022-05-27 02:29:30 +0300

    deleted front-end in this branch to check if i can merge

--- react-frontend/src/routes/home.jsx
+++
@@ -1,9 +0,0 @@
-import Box from '@mui/material/Box';
-
-const Home = () => {
-    return (
-        <Box height="100vh" display="flex" flexDirection="column" sx={{marginTop: 10}}>Home page</Box>
-    )
-}
-
-export default Home;
--- react-frontend/src/routes/login.jsx
+++
@@ -1,9 +0,0 @@
-import Box from '@mui/material/Box';
-
-const Login = () => {
-    return (
-        <Box height="100vh" display="flex" flexDirection="column"  sx={{marginTop: 10}}>Login page</Box>
-    )
-}
-
-export default Login;
