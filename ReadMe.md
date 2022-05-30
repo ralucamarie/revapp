f55859e39b5f39d8dcace0a4d89dbb8885136f22
Razvan Suhan <razvansuhan@yahoo.com>
2022-05-30 16:30:28 +0300

    Corrected from back-up

--- ReadMe.md
+++ ReadMe.md
@@ -4,3 +4,5 @@ Razvan Backend:
 -Updated revapp.sql with hard coded data for users, categories, shops and addresses:  
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Delete the revapp database from XAMPP mysql, create another database with the same name but without any tables  
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and import the file from this branch  
+
+-05.30.22 Brought back categories api files (careate, read, single_category, delete and update) from back-up version
--- php-backend/api/categories/read.php
+++ php-backend/api/categories/read.php
@@ -6,32 +6,29 @@ header("Content-Type: application/json;");
 include_once '../../models/category.php';
 include_once '../../config/database.php';
 
-
 $database = new Database();
 $db = $database->getConnection();
 
-$item = new Category($db);
+$items = new Category($db);
 
-$stmt = $item->getCategories();
+$stmt = $items->getCategories();
 $itemCount = $stmt->rowCount();
-
 
 if ($itemCount > 0) {
 
     $categoryArray = array();
 
+
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
         $e = array(
-            "id" => $item->id,
-            "category_name" => $item->category_name
+            "id" => $id,
+            "category_name" => $category_name
         );
+
         array_push($categoryArray, $e);
     }
-
     echo json_encode($categoryArray);
-    
-} 
-else {
+} else {
     echo json_encode("");
 }
