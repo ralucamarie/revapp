2128bbe4c0fee876b65c72d469832bea64fa25aa
Razvan Suhan <razvansuhan@yahoo.com>
2022-06-23 20:18:28 +0300

    applied backend back-up with correct files

--- php-backend/api/token/getUser.php
+++ php-backend/api/token/getUser.php
@@ -1,23 +1,24 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+//header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Headers: access");
+//header("Access-Control-Allow-Methods: GET");
+//header("Content-Type: application/json; charset=UTF-8");
+//header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+//
+//include_once '../../config/database.php';
+//include_once '../../api/AuthMiddleware1.php';
+//include_once '../permissions.php';
+//
+//$allHeaders = getallheaders();
+//$db_connection = new Database();
+//$db = $db_connection->getConnection();
+//$auth = new Auth($db, $allHeaders);
+//
+//
+//$permissions = LOGGED_IN;
+//
+//if($auth->isValid($permissions)["execute"])
+//echo json_encode($auth->isValid($permissions));
+//else echo json_encode("You do not have Authorization");
 
-include_once '../../config/database.php';
-include_once '../../api/AuthMiddleware1.php';
-include_once '../permissions.php';
-
-$allHeaders = getallheaders();
-$db_connection = new Database();
-$db = $db_connection->getConnection();
-$auth = new Auth($db, $allHeaders);
-
-
-$permissions = MODERATOR;
-
-if($auth->isValid($permissions)["execute"])
-echo json_encode($auth->isValid($permissions));
-else echo json_encode("You do not have Authorization");
 
