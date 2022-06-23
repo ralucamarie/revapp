7ab61e330631db02f542f9e13980caa5b2356d3b
Raluca Ceuca <ceuca.raluca@gmail.com>
2022-06-23 07:47:11 +0300

    small changes uncommited

--- php-backend/api/token/getUser.php
+++ php-backend/api/token/getUser.php
@@ -6,7 +6,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once '../../config/database.php';
-include_once './AuthMiddleware.php';
+include_once './../AuthMiddleware.php';
 
 $allHeaders = getallheaders();
 $db_connection = new Database();
---
+++ react-frontend/src/components/shops/add-shop.component.jsx
@@ -0,0 +1,24 @@
+import Button from "@material-ui/core/Button";
+import React, { useState } from "react";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import NativeSelect from "@mui/material/NativeSelect";
+import { styled } from "@mui/material/styles";
+
+
+const AddShop=()=>{
+
+    return(
+<Button variant="contained" color="primary">
+  Open Modal
+</Button>
+    )
+
+}
+
+export default AddShop
